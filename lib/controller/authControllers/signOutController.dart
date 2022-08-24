fd2fe12763a871c1916823af8d436702537f8fe7
anasfik <ffikhi.aanas@gmail.com>
2022-08-24 17:14:34 +0100

    commented the signOutController

--- lib/controller/authControllers/signOutController.dart
+++ lib/controller/authControllers/signOutController.dart
@@ -4,16 +4,26 @@ import 'package:get/get.dart';
 import '../functionsController/dialogsAndLoadingController.dart';
 
 class SignOutController extends GetxController {
+  // Dependency injection
   DialogsAndLoadingController dialogsAndLoadingController =
       Get.put(DialogsAndLoadingController());
-  // sign out from app
+
+  // Sign out from app
   signOut() async {
+    //
     try {
+      // Show loading
       dialogsAndLoadingController.showLoading();
 
+      // Sign out method
       await FirebaseAuth.instance.signOut();
+
+      // no need for pop because the AuthStateController will do it's work if it's successfully done
     } on FirebaseAuthException catch (e) {
+      // else, first pop
       Get.back();
+
+      // show error to user
       dialogsAndLoadingController.showError("Error: ${e.code}");
     }
   }
