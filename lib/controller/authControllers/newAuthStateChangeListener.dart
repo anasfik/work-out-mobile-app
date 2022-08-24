0dbbb4e6649f6b68b4ad4b2b895cc22d84326e07
anasfik <ffikhi.aanas@gmail.com>
2022-08-24 17:11:57 +0100

    Commeneted the newAuthStateChnageListener

--- lib/controller/authControllers/newAuthStateChangeListener.dart
+++ lib/controller/authControllers/newAuthStateChangeListener.dart
@@ -6,8 +6,27 @@ import 'package:work_out/view/screens/welcome/welcomePage.dart';
 import '../../view/screens/homepage/homePage.dart';
 
 class NewAuthStateChangeListener extends GetxController {
+  // Instance of the FirebaseAuth
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // An observable that can bu null
   Rxn<User?> user = Rxn<User?>(null);
+
+  // Auth state handler
+  handleTheUserState(User? user) {
+    if (user == null) {
+      // print("no user / user is signed out");
+      Get.offAll(WelcomePage());
+    } else {
+      if (!user.emailVerified) {
+        // print("user is signed in but not verified");
+        Get.offAll(EmailVerificatioPage());
+      } else {
+        // print("user is signed in");
+        Get.offAll(const HomePage());
+      }
+    }
+  }
 
   @override
   void onReady() {
@@ -15,20 +34,5 @@ class NewAuthStateChangeListener extends GetxController {
 
     user.bindStream(_auth.authStateChanges());
     super.onReady();
-  }
-
-  handleTheUserState(User? user) {
-    if (user == null) {
-      print("no user / user is signed out");
-      Get.offAll(WelcomePage());
-    } else {
-      if (!user.emailVerified) {
-        print("user is signed in but not verified");
-        Get.offAll(EmailVerificatioPage());
-      } else {
-        print("user is signed in");
-        Get.offAll(const HomePage());
-      }
-    }
   }
 }
