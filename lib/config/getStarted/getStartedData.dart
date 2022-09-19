f9c8d91f0c22029312d9665bfe22634fdf4f8b62
Gwhyyy <ffikhi.aanas@gmail.com>
2022-09-19 01:28:26 +0100

    [ Comment ] commented why I used factory instaed of creating direct list

--- lib/config/getStarted/getStartedData.dart
+++ lib/config/getStarted/getStartedData.dart
@@ -18,7 +18,13 @@ String cardsJson = '''[
     }
   ]''';
 
-// 
+// you can just make list of ChoiceChip directly like :
+// List<ChoiceCard> list = <ChoiceCard>[
+//   ChoiceCard(title: "title here", description: "description here"),
+//   ChoiceCard(title: "title here", description: "description here"),
+//   ChoiceCard(title: "title here", description: "description here"),
+// ];
+// but I just don't want to since this is personal issue LOL, so I genrate em with factory
 List<ChoiceCard> handledCardsList = List<ChoiceCard>.from(
   jsonDecode(cardsJson).map(
     (choiceCardJson) => ChoiceCard.fromJson(
